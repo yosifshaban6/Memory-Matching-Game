5fc6bd327d6bb53ba709b3dc02b52610185f49eb
Youssef Shaaban <yosifshaban6@gmail.com>
2025-04-01 23:31:03 +0200

    feat: Build game stats logic

--- src/scripts/playground-script.js
+++ src/scripts/playground-script.js
@@ -1,7 +1,9 @@
 const gameGrid = document.getElementById("game-grid");
-const params = new URLSearchParams(window.location.search);
+const resetButton = document.getElementById("reset-btn");
+const pauseButton = document.getElementById("pause-btn");
+const gameTimer = document.getElementById("timer");
+const moves = document.getElementById("moves");
 
-const difficulty = params.get("difficulty");
 let emojis = [
   "💐",
   "🌹",
@@ -36,6 +38,7 @@ let emojis = [
   "🎈",
   "🎈",
 ];
+
 // flip the card
 function flip(card) {
   card.style.transform = "rotateY(180deg)";
@@ -58,12 +61,17 @@ function removeCards(...arrOfCards) {
   }
 }
 
+function moveCounter() {
+  var movesCount = parseInt(moves.innerText);
+  moves.innerText = String(`${String(++movesCount).padStart(2, "0")}`);
+}
+
 let firstFlipCard = undefined;
 let secondFlipCard = undefined;
 //Handle the card flip logic
 function handleCardFlip(selectedCard) {
   // If a second card is flipped or the first card is clicked again
-  if (secondFlipCard || selectedCard === firstFlipCard) {
+  if (paused || secondFlipCard || selectedCard === firstFlipCard) {
     //do nothing
   }
   // If a first card is flipped
@@ -87,13 +95,14 @@ function handleCardFlip(selectedCard) {
         secondFlipCard = undefined;
       }, 1000);
     } else {
+      moveCounter();
       // Wait 1 second before flipping both cards back
       setTimeout(() => {
         flipBack(firstFlipCard);
         flipBack(secondFlipCard);
         firstFlipCard = undefined;
         secondFlipCard = undefined;
-      }, 1700);
+      }, 900);
     }
   }
   // If neither
@@ -119,13 +128,13 @@ function createCards(gridSize) {
   // Define grid size
   gameGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   // Define set of imojis to be used according to the grid size
-  emojis = emojis.sort(() => Math.random() - 0.5); // Shuffle the emojis
-  emojis = emojis.slice(0, (gridSize * gridSize) / 2); // Select half of the emojis
-  emojis = [...emojis, ...emojis]; // Duplicate the emojis
-  emojis = emojis.sort(() => Math.random() - 0.5); // Shuffle the emojis to ensure pairs are not necessarily on different half of cards
+  var gameEmojis = emojis.sort(() => Math.random() - 0.5); // Shuffle the emojis
+  gameEmojis = gameEmojis.slice(0, (gridSize * gridSize) / 2); // Select half of the emojis
+  gameEmojis = [...gameEmojis, ...gameEmojis]; // Duplicate the emojis
+  gameEmojis = gameEmojis.sort(() => Math.random() - 0.5); // Shuffle the emojis to ensure pairs are not necessarily on different half of cards
   // Append cards to the grid
   for (let i = 0; i < gridSize * gridSize; i++) {
-    imoji = emojis.pop();
+    imoji = gameEmojis.pop();
 
     gameGrid.innerHTML += `
         <div class="game-card ${cardSize}" id="${imoji}">
@@ -140,25 +149,78 @@ function createCards(gridSize) {
   }
 }
 
-switch (difficulty) {
-  case "easy-button":
-    // Set the grid to have 4 columns and Append 16 (4x4) cards
-    cards = createCards(4);
-    break;
-  case "medium-button":
-    // Set the grid to have 6 columns and Append 36 (6x6) cards
-    cards = createCards(6);
-    break;
-  case "hard-button":
-    // Set the grid to have 8 columns and Append 64 (8x8) cards
-    cards = createCards(8);
-    break;
-  default:
-    console.log("Unexpected difficulty level");
+var timerInterval;
+var elapsedSeconds = 0;
+function startTimer() {
+  clearInterval(timerInterval);
+
+  timerInterval = setInterval(function () {
+    elapsedSeconds++;
+    var minutes = Math.floor(elapsedSeconds / 60);
+    var remainingSeconds = Math.floor(elapsedSeconds % 60);
+
+    gameTimer.innerText = `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+  }, 1000);
 }
 
-for (let card of gameGrid.getElementsByClassName("game-card")) {
-  card.addEventListener("click", () => {
-    handleCardFlip(card);
-  });
+var paused = false;
+function pauseTimer() {
+  clearInterval(timerInterval);
+  paused = !paused;
+  if (paused) {
+    pauseButton.innerText = "Resume";
+  } else {
+    pauseButton.innerText = "Pause";
+    startTimer();
+  }
 }
+
+
+
+function startGame() {
+  startTimer();
+
+  // Get the difficulty level from the URL parameters
+  var params = new URLSearchParams(window.location.search);
+  var difficulty = params.get("difficulty");
+
+  switch (difficulty) {
+    case "easy-button":
+      // Set the grid to have 4 columns and Append 16 (4x4) cards
+      cards = createCards(4);
+      break;
+    case "medium-button":
+      // Set the grid to have 6 columns and Append 36 (6x6) cards
+      cards = createCards(6);
+      break;
+    case "hard-button":
+      // Set the grid to have 8 columns and Append 64 (8x8) cards
+      cards = createCards(8);
+      break;
+    default:
+      console.log("Unexpected difficulty level");
+  }
+
+  // Add event listeners to all cards
+  for (let card of gameGrid.getElementsByClassName("game-card")) {
+    card.addEventListener("click", () => {
+      handleCardFlip(card);
+    });
+  }
+}
+
+function resetGame() {
+  let firstFlipCard = undefined;
+  let secondFlipCard = undefined;
+  gameGrid.innerHTML = "";
+  elapsedSeconds = 0;
+  gameTimer.innerText = "00:00";
+  moves.innerText = "00";
+
+  pauseTimer();
+  startGame();
+}
+
+resetButton.addEventListener("click", resetGame);
+pauseButton.addEventListener("click", pauseTimer);
+startGame();
