f25f50badf9f1a58ccfc2c317ce040086e457ec9
Galaluddin Owais <galaluddinowais@gmail.com>
2025-04-02 03:43:21 +0200

    style: Enhance mode style

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,43 +1,24 @@
-
-const popup =   document.getElementById("popup")
-const easyButton =   document.getElementById("easy-button")
-const mediumButton =   document.getElementById("medium-button")
-const hardButton =   document.getElementById("hard-button")
-const difficultyToggle =   document.getElementById("difficulty-toggle")
-const difficultiesForm = document.getElementById("difficulties-form")
-const input = document.querySelector("[name=difficulty]")
+const popupContainer = document.getElementById("popup-container");
+const easyButton = document.getElementById("easy-button");
+const mediumButton = document.getElementById("medium-button");
+const hardButton = document.getElementById("hard-button");
+const difficultyToggle = document.getElementById("difficulty-toggle");
+const difficultiesForm = document.getElementById("difficulties-form");
+const input = document.querySelector("[name=difficulty]");
 
 function showPopup() {
-  popup.style.display = "flex";
+  popupContainer.style.display = "flex";
 }
 function hidePopup() {
-  popup.style.display = "none";
+  popupContainer.style.display = "none";
 }
 
-easyButton.addEventListener("mouseenter",()=>{
-  difficultyToggle.style.backgroundSize = "203%";
-})
+difficultiesForm.addEventListener("submit", (e) => {
+  input.value = e.submitter.id;
+});
 
-mediumButton.addEventListener("mouseenter",()=>{
-  difficultyToggle.style.backgroundSize = "136%";
-})
-
-hardButton.addEventListener("mouseenter",()=>{
-  difficultyToggle.style.backgroundSize = "100%";
-})
-
-difficultiesForm.addEventListener("mouseenter",()=>{
-  difficultyToggle.style.transition = "background-size 0.3s ease";
-})
-
-difficultiesForm.addEventListener("mouseleave",()=>{
-  difficultyToggle.style.backgroundSize = "410%"
-  difficultyToggle.style.transition = "background-size 0.3s ease";
-
-})
-
-
-difficultiesForm.addEventListener("submit",(e)=>{
-  input.value = e.submitter.id 
-  
-})
+popupContainer.addEventListener("click", (e) => {
+  if (e.target === popupContainer) {
+    hidePopup();
+  }
+});
